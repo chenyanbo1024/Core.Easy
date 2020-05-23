7bd79f43ce959f5e5509c1eb1ade738e9725e0a1
chenyanbo <chenyanbo1024@gmail.com>
2020-05-23 15:50:37 +0800

    优化代码,autofac注册提取

---
+++ Core.Web/Extensions/AutoFacModulesRegister.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Core.Web.Extensions
+{
+    public class AutoFacModulesRegister : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var basePath = AppContext.BaseDirectory;
+
+            var servicesDllFile = Path.Combine(basePath, "Core.Services.dll");
+            var repositoryDllFile = Path.Combine(basePath, "Core.Repository.dll");
+
+            if (!(File.Exists(servicesDllFile) && File.Exists(repositoryDllFile)))
+            {
+                throw new Exception("找不到 Core.Repository.dll 和 Core.Services.dll文件,确认文件位置");
+            }
+
+            //builder.RegisterType<LogAOP>();
+
+            // 获取 Services.dll 程序集服务，并注册
+            var assemblysServices = Assembly.LoadFrom(servicesDllFile);
+            builder.RegisterAssemblyTypes(assemblysServices)
+                   .AsImplementedInterfaces()
+                   .InstancePerDependency();
+            //.EnableInterfaceInterceptors()
+            //.InterceptedBy(typeof(LogAOP));
+
+            // 获取 Repository.dll 程序集服务，并注册
+            var assemblysRepository = Assembly.LoadFrom(repositoryDllFile);
+            builder.RegisterAssemblyTypes(assemblysRepository)
+                   .AsImplementedInterfaces()
+                   .InstancePerDependency();
+        }
+    }
+}
